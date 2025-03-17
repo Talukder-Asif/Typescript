bccc0ab1b45f5334963bb9f5247675d54ede6636
Talukder-Asif <asif.talukder.aj.10@gmail.com>
2025-03-17 12:45:01 +0600

    first commit

---
+++ app/basic.ts
@@ -0,0 +1,60 @@
+import { ArrayExpression } from "./../node_modules/acorn/dist/acorn.d";
+//Basic Data type
+const Name: string = "Asif Talukder";
+const age: number = 23;
+const isStudent: boolean = true;
+
+// Array
+const arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// tupple
+const info: [string, string, number, boolean, null] = [
+  "Asif",
+  "Atik",
+  2021,
+  true,
+  null,
+];
+
+// Object
+const person: {
+  name: string;
+  age: number;
+  isStudent: boolean;
+  address: string;
+} = {
+  name: "Asif",
+  age: 23,
+  isStudent: true,
+  address: "123 Main St, New York",
+};
+
+type StudentType = {
+  name: string;
+  age: number;
+  isStudent: boolean;
+  id: string;
+  batch: number;
+};
+
+const student1: StudentType = {
+  name: "Asif",
+  age: 23,
+  isStudent: true,
+  id: "123",
+  batch: 2021,
+};
+const student2: StudentType = {
+  name: "Rita",
+  age: 21,
+  isStudent: false,
+  id: "456",
+  batch: 2022,
+};
+
+// normal function and Array Function
+function multipy(num1: number, num2: number): number {
+  const result: number = num1 * num2;
+  return result;
+}
+console.log(multipy(2, 4));
---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const course = "Next Level Developer";
+console.log(course);
